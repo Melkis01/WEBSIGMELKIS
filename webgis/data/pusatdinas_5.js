9c12953d7235fd6a087fd5928a1b9adfd30a0c35
setianimulyandari <setiani.mulyandari@gmail.com>
2021-07-07 12:55:05 +0700

    uas melkis

---
+++ webgis/data/pusatdinas_5.js
@@ -0,0 +1,12 @@
+var json_pusatdinas_5 = {
+"type": "FeatureCollection",
+"name": "pusatdinas_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "kantor": "Dinas Kependudukan" }, "geometry": { "type": "Point", "coordinates": [ 111.041197312263009, -6.751727991152703 ] } },
+{ "type": "Feature", "properties": { "id": "2", "kantor": "Dinas Peternakan" }, "geometry": { "type": "Point", "coordinates": [ 111.027082197070328, -6.747718820885003 ] } },
+{ "type": "Feature", "properties": { "id": "3", "kantor": "Dinas Pertanian" }, "geometry": { "type": "Point", "coordinates": [ 111.029308523794782, -6.747989386138638 ] } },
+{ "type": "Feature", "properties": { "id": "4", "kantor": "Dinas Perhutanan" }, "geometry": { "type": "Point", "coordinates": [ 111.026661841674809, -6.749612774485464 ] } },
+{ "type": "Feature", "properties": { "id": "5", "kantor": "Dinas Perikanan" }, "geometry": { "type": "Point", "coordinates": [ 111.02321337055966, -6.754374682227432 ] } }
+]
+}
